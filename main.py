9b526e1c337969d68be9550a2cfbf909f2e5ed54
tidar-ouakrim <tidar.ouakrim@edu.esiee.fr>
2025-09-24 19:40:22 +0000

    message explicatif

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-#### Fonction secondaire
+"importe le module unicodedata"
+import unicodedata
 
+def ispalindrome(s):
+    'vérifie si une chaine de caractère est un palindrome'
+    s = unicodedata.normalize('NFD', s) # Décompose les caractères
+    s = ''.join(c for c in s if unicodedata.category(c) != 'Mn') # Supprime les accents
+    for i in [' ','+','.',',','?','!',':',';','_','-',"'",'()']:
+        s = s.replace(i,'') #Supprime les ponctuations et espaces
+    s = s.upper() #Met tout en majuscule
+    return s == s[::-1]  #Vérifie si c'est un palindrome
+      # votre code ici
 
-def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
 
 #### Fonction principale
 
 
 def main():
-
-    # vos appels à la fonction secondaire ici
+    'appels à la fonction secondaire'
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
